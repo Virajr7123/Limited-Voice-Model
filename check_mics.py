9803ec22f14641bf775d9cdbcf18f4d5817746b0
Viraj Sawant <105554788+Virajr7123@users.noreply.github.com>
2025-11-01 22:55:52 +0530

    Fix formatting issues in check_mics.py

--- check_mics.py
+++ check_mics.py
@@ -2,9 +2,9 @@ import speech_recognition as sr
 
 print("--- Finding all available microphones ---")
 
-# This will list every microphone Python can see
 for index, name in enumerate(sr.Microphone.list_microphone_names()):
     print(f"Mic index {index}: {name}")
 
 print("-----------------------------------------")
+
 print("\nLook for your 'real' microphone in the list and remember its index number.")
